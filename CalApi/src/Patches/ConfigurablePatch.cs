71b091542c2b2837246bbc70bcddeb991931f14b
ConfiG <cgytrus@cgyt.ru>
2021-10-27 17:51:09 +0300

    fix configurable patches config

--- CalApi/src/Patches/ConfigurablePatch.cs
+++ CalApi/src/Patches/ConfigurablePatch.cs
@@ -5,13 +5,13 @@ using BepInEx.Configuration;
 
 namespace CalApi.Patches {
     public abstract class ConfigurablePatch : IPatch {
-        protected static bool enabled { get; private set; }
+        protected bool enabled { get; private set; }
 
         protected ConfigurablePatch(ConfigFile config, string section, string key, bool defaultValue,
             string description) {
             if(string.IsNullOrWhiteSpace(key)) key = GetType().FullName;
-            if(config.Select(conf => conf.Key.Key).Contains(key))
-                throw new InvalidOperationException("Tried loading a fix which is already loaded.");
+            if(config.Select(conf => conf.Key).Any(def => def.Section == section && def.Key == key))
+                throw new InvalidOperationException("Tried loading a patch which is already loaded.");
 
             ConfigEntry<bool> configEntry = config.Bind(section, key, defaultValue, description);
             enabled = configEntry.Value;
