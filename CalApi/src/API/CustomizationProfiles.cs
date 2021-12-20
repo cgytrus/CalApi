25f1db1b7dad610f525ab4be21c002e264efd517
ConfiG <cgytrus@cgyt.ru>
2021-12-20 18:52:44 +0300

    what was i even thinking

--- CalApi/src/API/CustomizationProfiles.cs
+++ CalApi/src/API/CustomizationProfiles.cs
@@ -14,13 +14,19 @@ public static class CustomizationProfiles {
         Path.Combine(Util.moddedStreamingAssetsPath, "Customization Profiles");
 
     public static readonly string defaultPath = Path.Combine(profilesPath, DefaultProfile);
-    public static readonly string currentPath = Path.Combine(profilesPath, _customizationProfile!.Value);
+    public static string? currentPath;
     public static event EventHandler? profileChanged;
 
     internal static void LoadSettings(ConfigFile config) {
         _customizationProfile = config.Bind("General", "CustomizationProfile", DefaultProfile,
             "The customization profile name to be used by mods for customizing certain aspects of the mod or the game.");
 
-        _customizationProfile.SettingChanged += (_, _) => profileChanged?.Invoke(null, EventArgs.Empty);
+        UpdateCurrentPath();
+        _customizationProfile.SettingChanged += (_, _) => {
+            UpdateCurrentPath();
+            profileChanged?.Invoke(null, EventArgs.Empty);
+        };
     }
+
+    private static void UpdateCurrentPath() => currentPath = Path.Combine(profilesPath, _customizationProfile!.Value);
 }
