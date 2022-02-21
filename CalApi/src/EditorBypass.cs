7d458e1ce9f58eaffdf52c49c3ed2cee85699f6b
ConfiG <cgytrus@cgyt.ru>
2022-02-21 20:26:05 +0300

    verification bypass

--- CalApi/src/EditorBypass.cs
+++ CalApi/src/EditorBypass.cs
@@ -1,8 +1,16 @@
-﻿using BepInEx.Configuration;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using BepInEx.Configuration;
+
+using HarmonyLib;
 
 using Mono.Cecil.Cil;
 
 using MonoMod.Cil;
+
+using UnityEngine;
 
 namespace CalApi;
 
@@ -13,6 +21,7 @@ public class EditorBypass {
     private readonly ConfigEntry<bool> _itemCostLimitBypass;
     private readonly ConfigEntry<bool> _banListsBypass;
     private readonly ConfigEntry<bool> _maxRoomWorldCountBypass;
+    private readonly ConfigEntry<bool> _verificationBypass;
 
     public EditorBypass(ConfigFile config) {
         _clippingItemsBypass = config.Bind(SectionName, "ClippingItemsBypass", false, "");
@@ -20,6 +29,7 @@ public class EditorBypass {
         "Required to be enabled for players as well!");
         _banListsBypass = config.Bind(SectionName, "BanListsBypass", false, "");
         _maxRoomWorldCountBypass = config.Bind(SectionName, "MaxRoomWorldCountBypass", false, "");
+        _verificationBypass = config.Bind(SectionName, "VerificationBypass", false, "");
     }
 
     public void Load() {
@@ -40,5 +50,53 @@ public class EditorBypass {
                 cursor.Emit(OpCodes.And);
             }
         };
+
+        VerificationBypass();
+    }
+
+    private void VerificationBypass() {
+        On.RoomEditorSharePlaytest.StartPlaytest += orig => {
+            if(!_verificationBypass.Value) {
+                orig();
+                return;
+            }
+
+            AccessTools.Property(typeof(RoomEditorSharePlaytest), nameof(RoomEditorSharePlaytest.PlaytestActive))
+                .SetValue(null, true);
+            Door.CommunityPackCompletedAction?.Invoke();
+        };
+
+        IL.RoomEditorSharePlaytest.SetProgressionArrays += il => {
+            ILCursor cursor = new(il);
+            cursor.GotoNext(code => code.MatchStloc(3));
+            cursor.Index -= 2;
+            cursor.EmitReference(this);
+            cursor.Index++;
+            cursor.Emit(OpCodes.Ldloc_0);
+            cursor.Emit<EditorBypass>(OpCodes.Call, nameof(GeneratePackProgression));
+        };
+    }
+
+    private Dictionary<string, List<string>> GeneratePackProgression(
+        Dictionary<string, List<string>> progressionGuids, string packPath) {
+        if(!_verificationBypass.Value) return progressionGuids;
+        progressionGuids.Clear();
+
+        string[] packProgression = ProgressionArrayGenerator.GetPackProgressionArray(packPath, false);
+        Dictionary<string, List<string>> worldProgressions = new(packProgression.Length);
+
+        foreach(string worldPath in Directory.EnumerateDirectories(packPath)) {
+            string worldSettingsPath = Path.Combine(worldPath, "settings.data");
+            if(!File.Exists(worldSettingsPath)) continue;
+
+            WorldSettings.Settings worldSettings =
+                JsonUtility.FromJson<WorldSettings.Settings>(File.ReadAllText(worldSettingsPath));
+
+            string[] worldProgression = ProgressionArrayGenerator.GetWorldProgressionArray(worldPath);
+            worldProgressions.Add(worldSettings.worldGUID, worldProgression.ToList());
+        }
+
+        foreach(string worldGuid in packProgression) progressionGuids.Add(worldGuid, worldProgressions[worldGuid]);
+        return progressionGuids;
     }
 }
