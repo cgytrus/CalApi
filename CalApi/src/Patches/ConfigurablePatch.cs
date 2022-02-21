80dc371bf2ed63569f38c573f4e04d2377e82600
ConfiG <cgytrus@cgyt.ru>
2022-02-21 17:25:10 +0300

    description can't be null

--- CalApi/src/Patches/ConfigurablePatch.cs
+++ CalApi/src/Patches/ConfigurablePatch.cs
@@ -9,13 +9,13 @@ namespace CalApi.Patches;
 public abstract class ConfigurablePatch : IPatch {
     protected bool enabled { get; private set; }
 
-    protected ConfigurablePatch(ConfigFile config, string? section, string? key, bool defaultValue,
+    protected ConfigurablePatch(ConfigFile config, string section, string? key, bool defaultValue,
         string? description) {
         if(string.IsNullOrWhiteSpace(key)) key = GetType().FullName;
         if(config.Select(conf => conf.Key).Any(def => def.Section == section && def.Key == key))
             throw new InvalidOperationException("Tried loading a patch which is already loaded.");
 
-        ConfigEntry<bool> configEntry = config.Bind(section, key, defaultValue, description);
+        ConfigEntry<bool> configEntry = config.Bind(section, key, defaultValue, description ?? "");
         enabled = configEntry.Value;
         configEntry.SettingChanged += (_, _) => { enabled = configEntry.Value; };
     }
